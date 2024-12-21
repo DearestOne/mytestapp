a89e16e010424c8e95cdde153ee3462c56934fc5
Polla <122893119+DearestOne@users.noreply.github.com>
2024-12-21 13:19:53 +0700

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("CPSU project",
+         style: GoogleFonts.aladin(),
+         ),
+        backgroundColor: Colors.redAccent,
+        centerTitle: true,
+      ),
+      body: 
+        Column( 
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [ Container(
+              padding: const EdgeInsets.all(30),
+              color: Colors.amber,
+              child: const Text("hello world"),
+            ),
+            Container(
+              padding: const EdgeInsets.all(30),
+              color: Colors.amber,
+              child: const Text("hello world"),
+            ),
+            Container(
+              padding: const EdgeInsets.all(30),
+              color: Colors.amber,
+              child: const Text("hello world"),
+            )],),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [ Container(
+              padding: const EdgeInsets.all(30),
+              color: Colors.amber,
+              child: const Text("hello world"),
+            ),
+            Container(
+              padding: const EdgeInsets.all(30),
+              color: Colors.amber,
+              child: const Text("hello world"),
+            ),
+            Container(
+              padding: const EdgeInsets.all(30),
+              color: Colors.amber,
+              child: const Text("hello world"),
+            )],),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [ Container(
+              padding: const EdgeInsets.all(30),
+              color: Colors.amber,
+              child: const Text("hello world"),
+            ),
+            Container(
+              padding: const EdgeInsets.all(30),
+              color: Colors.amber,
+              child: const Text("hello world"),
+            ),
+            Container(
+              padding: const EdgeInsets.all(30),
+              color: Colors.amber,
+              child: const Text("hello world"),
+            )],),
+          ]
+        )
+    );
+  }
+}
